292460f502c69f0e6dce75439b495682f098b63e
sujivennapusa <vennapusasuji463@gmail.com>
2021-07-21 19:27:22 +0530

    largest of three numbers

---
+++ large3.py
@@ -0,0 +1,12 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number"))
+if(a>b and a>c):
+    print(a)
+elif(b>a and b>c):
+    print(b)
+else:
+    print(c)    
+
+
+    
