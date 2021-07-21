f3bc2024e8919a242473d310a9af340c6f28fadb
sujivennapusa <vennapusasuji463@gmail.com>
2021-07-21 19:34:23 +0530

    even or odd

---
+++ check.py
@@ -0,0 +1,5 @@
+a=int(input("enter the first number"))
+if(a%2==0):
+    print("even")
+else:
+    print("odd")    
---
+++ small3.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number"))
+if(a<b&a<c):
+    print(a)
+elif(b<a&b<c):
+    print(b)
+else:
+    print(c)    
+
